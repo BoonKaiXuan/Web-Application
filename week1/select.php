47c590d2928dfa4ec3428471df0b18a519155118
BoonKaiXuan <kaixuan251002@dasein.edu.my>
2026-06-09 10:57:29 +0800

    feat: created new select file

---
+++ week1/select.php
@@ -0,0 +1,29 @@
+<?php
+$servername = "localhost";
+$username = "aliceboon";
+$password = "GFn/4dHUq(39b_d@";
+$dbname = "aliceboon";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT * FROM student";
+// Execute the SQL query
+$result = $conn->query($sql);
+
+// Process the result set
+if ($result->num_rows > 0) {
+  // Output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo "Name: " . $row["name"]. " - Email: " . $row["email"]. "<br>";
+  }
+} else {
+  echo "0 results";
+}
+
+$conn->close();
+?>
