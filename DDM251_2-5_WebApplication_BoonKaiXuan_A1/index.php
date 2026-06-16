7471376eb3592962b58a3c46adbed2f8c41c2750
BoonKaiXuan <kaixuan251002@dasein.edu.my>
2026-06-16 23:00:32 +0800

    style: styled login pg

--- DDM251_2-5_WebApplication_BoonKaiXuan_A1/index.php
+++ DDM251_2-5_WebApplication_BoonKaiXuan_A1/index.php
@@ -4,7 +4,7 @@ $username = "aliceshop";
 $password = "E1yYuo(k47nHG(T9";
 $dbname = "aliceshop";
 
-//$error_message = "";
+$error_message = "";
 $keep_username = "";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -14,12 +14,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         die("Connection failed: " . $conn->connect_error);
     }
 
-    if (isset($_POST['username'])) {
+    if (!empty($_POST['username'])) {
         $keep_username = htmlspecialchars($_POST['username']);
     }
 
     if (empty($_POST['username']) || empty($_POST['password'])) {
-        echo "*Please enter your username and password.";
+        $error_message = "*Please enter your Username and Password.";
     } else {
         $username = $_POST['username'];
         $password = $_POST['password'];
@@ -34,10 +34,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             if ($password == $user['password']) {
                 header("Location:welcome.php");
             } else {
-                echo "*Invalid password. Please try again.";
+                $error_message = "*Invalid password. Please try again.";
             }
         } else {
-            echo "*Invalid username. Please try again.";
+            $error_message = "*Invalid username. Please try again.";
         }
     }
 
@@ -53,12 +53,102 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome to Alice Shop - Sign In</title>
+    <title>Welcome to Alice's Shop - Sign In</title>
 </head>
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+
+    * {
+        margin: 0;
+        box-sizing: border-box;
+    }
+
+    body {
+        margin: 60px;
+        font-family: "Poppins", sans-serif;
+        font-size: 16px;
+        font-weight: 300;
+        background-color: #FCF5EE;
+    }
+
+    #login {
+        background-color: #FFEF9F;
+        max-width: 500px;
+        margin: auto;
+        padding: 40px;
+        border-radius: 20px;
+    }
+
+    form {
+        margin: auto;
+    }
+
+    h1 {
+        color: #E13F7C;
+    }
+
+    .header {
+        text-align: center;
+        margin-bottom: 50px;
+    }
+
+    .login_info {
+        margin-bottom: 40px;
+    }
+
+    .login_info>div {
+        display: flex;
+        gap: 20px;
+        padding: 10px 0;
+        align-items: center;
+    }
+
+    .login_info>div label {
+        width: 30%;
+        font-size: 18px;
+        font-weight: 600;
+    }
+
+    .login_info>div input {
+        width: 65%;
+        height: 40px;
+        background-color: #ffffffc7;
+        border-radius: 8px;
+        border: none;
+        padding: 0px 10px;
+    }
+
+    .login_info>div input:focus {
+        outline: none;
+    }
+
+    .btn {
+        background-color: #E13F7C;
+        border: none;
+        padding: 15px 40px;
+        font-size: 16px;
+        font-weight: 600;
+        color: #FFEF9F;
+        border-radius: 10px;
+        width: 100%;
+        margin: 10px 0px;
+        cursor: pointer;
+    }
+
+    .error-msg {
+        padding-bottom: 30px;
+        color: #ff285a;
+        font-weight: 600;
+    }
+
+    .error-msg:blank {
+        display: none;
+    }
+</style>
 
 <body>
-    <div>
-        <h1>Welcome to Alice Shop</h1>
+    <div class="header">
+        <h1>Welcome to Alice's Shop</h1>
         <p>
             Please sign in to continue shopping with us!
         </p>
@@ -66,19 +156,25 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     <div id="login">
         <form taget="_self" method="POST">
+            <div class="error-msg">
+                <?php
+                echo $error_message;
+                ?>
+            </div>
+            <div class="login_info">
+                <div>
+                    <label>Username:</label>
+                    <input type="text" name="username" value="<?php echo $keep_username; ?>">
+                </div>
 
-            <div>
-                <h2>Username:</h2>
-                <input type="text" name="username">
+                <div>
+                    <label>Password:</label>
+                    <input type="password" name="password">
+                </div>
             </div>
 
             <div>
-                <h2>Password:</h2>
-                <input type="password" name="password">
-            </div>
-
-            <div>
-                <input type="submit" value="Sign In">
+                <input class="btn" type="submit" value="Sign In">
             </div>
 
         </form>
