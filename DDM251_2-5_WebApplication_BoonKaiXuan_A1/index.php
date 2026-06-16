a27d7fffaa7cf6bbc29260f22e7f149c1a753ce3
BoonKaiXuan <kaixuan251002@dasein.edu.my>
2026-06-16 18:11:50 +0800

    feat: created new index.php file

---
+++ DDM251_2-5_WebApplication_BoonKaiXuan_A1/index.php
@@ -0,0 +1,29 @@
+<?php
+$servername = "localhost";
+$username = "aliceshop";
+$password = "E1yYuo(k47nHG(T9 ";
+$dbname = "aliceshop";
+
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Page - Alice Shop</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
