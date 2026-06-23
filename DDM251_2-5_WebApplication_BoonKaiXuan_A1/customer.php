358124947bb469eae8d996a9cab993c7da00e904
BoonKaiXuan <kaixuan251002@dasein.edu.my>
2026-06-23 18:13:30 +0800

    style: styled customer listing pg

--- DDM251_2-5_WebApplication_BoonKaiXuan_A1/customer.php
+++ DDM251_2-5_WebApplication_BoonKaiXuan_A1/customer.php
@@ -19,6 +19,9 @@ if ($conn->connect_error) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Customer List - Alice's Shop</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,1,0&icon_names=dashboard" />
+    <script src="https://kit.fontawesome.com/1619a0e9db.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="css/common.css">
 </head>
 
 <style>
@@ -34,73 +37,75 @@ if ($conn->connect_error) {
 </style>
 
 <body>
-    <div class="sidebar">
-        <div class="sidebar_header">
-            Alice's Shop
+    <div class="container">
+        <div class="sidebar">
+            <div class="sidebar_header">
+                Alice's Shop
+            </div>
+            <div class="sidebar_menu vert-align">
+                <span class="material-symbols-outlined">
+                    dashboard
+                </span>Dashboard
+            </div>
+            <div class="sidebar_menu sidebar_menu_active">
+                <i class="fa-solid fa-user"></i>
+                Customers
+            </div>
+            <div class="sidebar_menu">
+                <i class="fa-solid fa-box-open"></i>
+                Products
+            </div>
+            <div class="sidebar_menu">
+                <i class="fa-solid fa-cart-shopping"></i>
+                Orders
+            </div>
+            <div class="sidebar_menu">
+                <i class="fa-solid fa-right-from-bracket"></i>
+                Sign Out
+            </div>
         </div>
-        <div class="sidebar_menu sidebar_menu_active vert-align">
-            <span class="material-symbols-outlined">
-                dashboard
-            </span>Dashboard
-        </div>
-        <div class="sidebar_menu">
-            <i class="fa-solid fa-user"></i>
-            Customers
-        </div>
-        <div class="sidebar_menu">
-            <i class="fa-solid fa-box-open"></i>
-            Products
-        </div>
-        <div class="sidebar_menu">
-            <i class="fa-solid fa-cart-shopping"></i>
-            Orders
-        </div>
-        <div class="sidebar_menu">
-            <i class="fa-solid fa-right-from-bracket"></i>
-            Sign Out
-        </div>
-    </div>
 
-    <div>
-        <table width="1100">
-            <tr>
-                <th>Customer ID</th>
-                <th width="200">Username</th>
-                <th width="200">First Name</th>
-                <th width="200">Last Name</th>
-                <th width="300">Email</th>
-            </tr>
-
-            <?php
-            $query = "SELECT * FROM customers";
-
-            $result = mysqli_query($conn, $query);
-
-            while ($row = mysqli_fetch_assoc($result)) {
-            ?>
-
+        <div class="main">
+            <table width="1100">
                 <tr>
-                    <td><?php echo $row['customerID']; ?></td>
-                    <td><?php echo $row['username']; ?></td>
-                    <td><?php echo $row['firstName']; ?></td>
-                    <td><?php echo $row['lastName']; ?></td>
-                    <td><?php echo $row['customerEmail']; ?></td>
-                    <td>
-                        <a href="customerDetails.php">
-                            <input type='button' value='Details'>
-                        </a>
-                    </td>
-                    <td><input type='button' value='Edit'></td>
-                    <td><button>Delete</button></td>
+                    <th>Customer ID</th>
+                    <th width="200">Username</th>
+                    <th width="200">First Name</th>
+                    <th width="200">Last Name</th>
+                    <th width="300">Email</th>
                 </tr>
-            <?php
-            }
-            mysqli_close($conn);
-            ?>
 
-            <a href="addCustomer.php"><input type="submit" value="AddCustomer"></a>
+                <?php
+                $query = "SELECT * FROM customers";
 
-        </table>
+                $result = mysqli_query($conn, $query);
+
+                while ($row = mysqli_fetch_assoc($result)) {
+                ?>
+
+                    <tr>
+                        <td><?php echo $row['customerID']; ?></td>
+                        <td><?php echo $row['username']; ?></td>
+                        <td><?php echo $row['firstName']; ?></td>
+                        <td><?php echo $row['lastName']; ?></td>
+                        <td><?php echo $row['customerEmail']; ?></td>
+                        <td>
+                            <a href="customerDetails.php">
+                                <input type='button' value='Details'>
+                            </a>
+                        </td>
+                        <td><input type='button' value='Edit'></td>
+                        <td><button>Delete</button></td>
+                    </tr>
+                <?php
+                }
+                mysqli_close($conn);
+                ?>
+
+                <a href="addCustomer.php"><input class="btn" h type="submit" value="AddCustomer"></a>
+
+            </table>
+        </div>
     </div>
 
 </body>
--- DDM251_2-5_WebApplication_BoonKaiXuan_A1/welcome.php
+++ DDM251_2-5_WebApplication_BoonKaiXuan_A1/welcome.php
@@ -19,94 +19,17 @@ if ($conn->connect_error) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Welcome to Alice's Shop</title>
+    <link rel="stylesheet" href="css/common.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,1,0&icon_names=dashboard" />
     <script src="https://kit.fontawesome.com/1619a0e9db.js" crossorigin="anonymous"></script>
 </head>
-
 <style>
-    @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
-
-    * {
-        margin: 0;
-        box-sizing: border-box;
-    }
-
-    body {
-        margin: 20px 30px;
-        font-family: "Poppins", sans-serif;
-        font-size: 16px;
-        font-weight: 300;
-        background-color: #fffbfb;
-    }
-
-    h1 {
-        color: #E13F7C;
-        font-size: 48px;
-        font-weight: 700;
-    }
-
-    .container {
-        max-width: 1520px;
-        margin: auto;
-        display: flex;
-        gap: 10px;
-    }
-
-    .main {
-        padding: 30px;
-    }
-
-    .sidebar {
-        min-width: 130px;
-        height: 95vh;
-        background-color: #FFEF9F;
-        color: #E13F7C;
-        font-weight: 600;
-        padding: 20px 10px;
-        border-radius: 10px;
-    }
-
-    .sidebar_header {
-        text-align: center;
-        padding: 20px 0px 50px 0px;
-        font-size: 20px;
-    }
-
-    .sidebar>div i,
-    .sidebar>div span {
-        color: #E13F7C;
-        font-size: 20px;
-        margin-right: 10px;
-    }
-
-    .sidebar>div span {
-        font-size: 24px;
-    }
-
-    .sidebar_menu {
-        padding: 20px 28px;
-        margin: 10px 0px;
-    }
-
-    .sidebar_menu:hover,
-    .sidebar_menu_active {
-        cursor: pointer;
-        background-color: #E13F7C;
-        border-radius: 5px;
-    }
-
-    .sidebar_menu:hover,
-    .sidebar_menu:hover i,
-    .sidebar_menu:hover span,
-    .sidebar_menu_active,
     .sidebar>div span {
         color: #FFEF9F;
     }
 
-    .vert-align {
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
+    .btn {
+        width: 100%;
     }
 </style>
 
