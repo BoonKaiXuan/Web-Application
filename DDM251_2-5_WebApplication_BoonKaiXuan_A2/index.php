211d095ffb18016450e6bb09a49e124ee10a1320
BoonKaiXuan <kaixuan251002@dasein.edu.my>
2026-07-21 07:41:32 +0800

    feat: created new index.php file with registration details

---
+++ DDM251_2-5_WebApplication_BoonKaiXuan_A2/index.php
@@ -0,0 +1,126 @@
+<?php
+$servername = "localhost";
+$username = "tealive";
+$password = "5spY@)Hmeg]XrKeS";
+$dbname = "tealive";
+
+//if ($_SERVER["REQUEST_METHOD"] == "POST") {
+if (isset($_POST["email"]) && isset($_POST["password"])) {
+    // Get user input from the form
+    $userEmail = $_POST['email'];
+    $userPassword = $_POST['password'];
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    session_start();
+
+    //Execute sql query
+    $sql = "SELECT * FROM student WHERE email = '$userEmail' AND password = '$userPassword'";
+
+    $result = $conn->query($sql);
+
+    //Process the result set
+    if ($result->num_rows > 0) {
+        //echo "Login Successful";
+        $_SESSION['email'] = $_POST['email'];
+        header("Location:booklist.php");
+    } else {
+        echo "User Not Found";
+    }
+
+    $conn->close();
+}
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register/Login - Tealive New Product Launch</title>
+
+    <style>
+        * {
+            font-size: 20px;
+        }
+
+        body {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+        }
+    </style>
+
+</head>
+
+<body>
+
+    <div>
+        <h1>Seek, Sip & Win Big!</h1>
+        <p>Register to find our hidden new drink to snag the BIG prize! Missed it? Don’t worry, you still walk away with a sweet treat.</p>
+
+        <div>
+            <form taget="_self" method="POST">
+                <!--
+                <div class="error-msg">
+                    <?php
+                    echo $error_message;
+                    ?>
+                </div>
+-->
+                <div class="login_info">
+                    <div>
+                        <label>First Name:</label>
+                        <input type="text" name="firstName">
+                    </div>
+
+                    <div>
+                        <label>Last Name:</label>
+                        <input type="text" name="lastName">
+                    </div>
+
+                    <div>
+                        <label>Email:</label>
+                        <input type="text" name="email">
+                    </div>
+
+                    <div>
+                        <label>Contact No.:</label>
+                        <input type="text" name="contactNo">
+                    </div>
+
+                    <div>
+                        <label>Password:</label>
+                        <input type="password" name="password">
+                    </div>
+
+                    <div>
+                        <label>Confirm Password:</label>
+                        <input type="password" name="confirmPassword">
+                    </div>
+                </div>
+
+                <div>
+                    <a href="">Join the Hunt & Claim Perk</a>
+                </div>
+
+            </form>
+            <a href="">
+                Already a Tealive App user? Sign in here.
+            </a>
+        </div>
+
+    </div>
+
+</body>
+
+</html>
