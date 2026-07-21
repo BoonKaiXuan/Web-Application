cc289c08f581a828bf28454eb5c4bd1a212f4df2
BoonKaiXuan <kaixuan251002@dasein.edu.my>
2026-07-21 10:37:39 +0800

    feat: created survey questions pg

---
+++ DDM251_2-5_WebApplication_BoonKaiXuan_A2/survey.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Survey Question 1 - Tealive New Product Launch</title>
+</head>
+
+<body>
+    <!-- Progress Bar -->
+    <div>
+        <progress value="1" max="3"></progress>
+    </div>
+
+    <div>
+        <h1>Q1: What kind of drink are you craving today?</h1>
+        <select>
+            <option>Rich & Creamy</option>
+            <option>Fruity & Sweet</option>
+            <option>Refreshing & Tangy</option>
+            <option>Light & Floral</option>
+        </select>
+    </div>
+
+    <a href="survey_Q2.php">
+        Next
+    </a>
+</body>
+
+</html>
---
+++ DDM251_2-5_WebApplication_BoonKaiXuan_A2/survey_Q3.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Survey Question 3 - Tealive New Product Launch</title>
+</head>
+
+<body>
+    <a href="survey_Q2.php">Back</a>
+    <!-- Progress Bar -->
+    <div>
+        <progress value="3" max="3"></progress>
+    </div>
+
+    <div>
+        <h1>Q3: Which drink would you choose on a hot day?</h1>
+        <select>
+            <option>Brown Sugar Milk Tea</option>
+            <option>Strawberry Milk Tea</option>
+            <option>Tropical Fruit Tea</option>
+            <option>Floral Fruit Tea</option>
+        </select>
+    </div>
+
+    <a href="drink_result.php">
+        Show My Drink
+    </a>
+</body>
+
+</html>
