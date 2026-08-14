056b69e3749631661fd65fc279e0c278aba142bb
BoonKaiXuan <kaixuan251002@dasein.edu.my>
2026-08-14 19:19:44 +0800

    feat: week11 in-class exe draft

---
+++ Week11/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Week11</title>
+</head>
+
+<body>
+    <div>
+        <form action="process-login.php" method="POST">
+            <h2>Name:</h2>
+            <input type="text" name="name">
+            <br>
+            <h2>Email:</h2>
+            <input type="text" name="email">
+            <br>
+            <h2>Age</h2>
+            <input type="number" name="age">
+            <br>
+            <br>
+            <input type="submit">
+        </form>
+    </div>
+
+
+</body>
+
+</html>
